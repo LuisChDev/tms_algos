eca1fe687f981b4040cc2f664613fd166921913b
Luis Chavarriaga <luischa123@gmail.com>
2022-05-14 15:52:42 -0500

    switch to nightly rust

---
+++ README.md
@@ -0,0 +1,4 @@
+# tms_algos
+
+This project is about providing meta-heuristic algorithms for use in a Transportation Management System (TMS).
+
--- flake.nix
+++ flake.nix
@@ -1,22 +1,41 @@
 {
   description = "routing metaheuristics";
-  nixConfig.bash-prompt = "e[1;34m [TMSalgos@$HOSTNAME:$PWD]$ e[0m";
+  nixConfig.bash-prompt = "\\e[1;34m\n\[tms_algos@\$HOSTNAME:\$PWD\]$ \\e[0m";
 
   inputs = {
     nixpkgs.url =
       "github:NixOS/nixpkgs?rev=46821ea01c8f54d2a20f5a503809abfc605269d7";
+    rust-overlay.url =
+      "github:oxalica/rust-overlay?rev=43f4c4319fd29d07912a65d405ff03069c7748c4";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlay ];
+        };
         packageName = "TMSalgos";
 
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ cargo cargo-edit rustc rustfmt rust-analyzer ];
+          nativeBuildInputs = with pkgs; [
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+          ];
+
+          buildInputs = with pkgs; [
+            # cargo
+            # cargo-edit
+            # rustc
+            # rustfmt
+            # rust-analyzer
+
+            # debugger support
+            gdb
+            gdbgui
+          ];
         };
       });
 }
