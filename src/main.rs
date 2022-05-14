58280f3aea179b9df9df119fbd61c245f65c73b7
Luis Chavarriaga <luischa123@gmail.com>
2022-05-14 17:14:01 -0500

    refactor main, add e2e tests

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,42 @@
-mod solution;
-mod params;
+#![feature(proc_macro_hygiene, decl_macro)]
 
+//! This file contains the entrypoint to our service.
+
+// data models
+mod params;
+mod solution;
+
+// algorithms
 mod ant_colony_optimization;
 mod simulated_annealing;
 
+// other
+mod testing;
+
 use rand::prelude::*;
+use rocket::*;
+use rocket_lamb::RocketExt;
+use rocket::serde::json::Json;
 
-use crate::solution::Solution;
-
-
-fn main() {
-  println!("calculating optimal solution.");
-
-  let _matr = vec![
-    vec![1.0, 2.0, 3.0, 15.0],
-    vec![4.0, 5.0, 6.0, 17.0],
-    vec![7.0, 8.0, 9.0, 12.0],
-    vec![7.0, 8.0, 9.0, 9.0]
-  ];
-
-  let mut rng = thread_rng();
-  let mut large_matrix: Vec<Vec<f64>> = Vec::new();
-  for _ in 0..1000 {
-    let mut row: Vec<f64> = Vec::new();
-    for _ in 0..1000 {
-      row.push(rng.gen_range(0.0..1000.0));
-    }
-    large_matrix.push(row);
-  }
+use crate::solution::{Solution, TSP};
 
 
-  println!("creating solution model.");
-  let mut sol = solution::TSP::new(large_matrix, None);
-  let sol2 = sol.clone();
+#[get("/")]
+fn index() -> &'static str {
+  "Hello, world!"
+}
 
-  // println!("optimizing using annealing");
-  // simulated_annealing::anneal(&mut sol);
+#[get("/test")]
+fn testing() -> Json<TSP> {
+  Json(basic_tsp())
+}
 
-  println!("optimizing using ant colony optimization.");
-  let result = ant_colony_optimization::ant_colony_optimize(&sol2, 0.1);
-  println!("optimal result is {:?}", result);
+#[post("/anneal", format = "json", data = "<problem>")]
+fn anneal(problem: rocket::serde::json::Json<TSP>) -> String {
+  let parsed: TSP = problem.0;
+  format!("the cities, in order, are {:?} and the matrix is {:?}", parsed.locs(), parsed.distances())
+}
+
+fn main() -> _ {
+  rocket::ignite().mount("/", routes![index, testing, anneal])
 }
---
+++ src/testing.rs
@@ -0,0 +1,46 @@
+fn basic_tsp() -> TSP {
+  let matr = vec![
+    vec![0.0, 22.0, 3.0, 15.0],
+    vec![4.0, 0.0, 60.0, 17.0],
+    vec![7.0, 8.0, 0.0, 125.0],
+    vec![71.0, 8.0, 9.0, 0.0],
+  ];
+
+  solution::TSP::new(matr, None)
+}
+
+fn calculating() {
+  println!("calculating optimal solution.");
+
+  let matr = vec![
+    vec![0.0, 22.0, 3.0, 15.0],
+    vec![4.0, 0.0, 60.0, 17.0],
+    vec![7.0, 8.0, 0.0, 125.0],
+    vec![71.0, 8.0, 9.0, 0.0],
+  ];
+
+  let mut rng = thread_rng();
+  let mut _large_matrix: Vec<Vec<f64>> = Vec::new();
+  for _ in 0..1000 {
+    let mut row: Vec<f64> = Vec::new();
+    for _ in 0..1000 {
+      row.push(rng.gen_range(0.0..1000.0));
+    }
+    _large_matrix.push(row);
+  }
+
+  println!("creating solution model.");
+  let mut sol = solution::TSP::new(matr, None);
+  let sol2 = sol.clone();
+
+  println!("optimizing using annealing");
+  simulated_annealing::anneal(&mut sol);
+
+  println!("optimizing using ant colony optimization.");
+  let result = ant_colony_optimization::ant_colony_optimize(&sol2, 0.1);
+  println!("optimal result is {:?}", result);
+  println!(
+    "cost for this result is {:?}",
+    TSP::calc_cost(&result, sol2.distances())
+  )
+}
